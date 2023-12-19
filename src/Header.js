3bdcdbfc067d4ddfd56de3d3941f583ef36e6ed6
sanraj98 <santhoshrajrathinam@gmail.com>
2023-12-19 08:20:52 +0530

    modify

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import "./header.css";
+import { CiMail } from "react-icons/ci";
+import { MdOutlinePhone } from "react-icons/md";
+
+
+
 
 const Header = () => {
+  //const title="Dhana Raksha";
+  const mail="dhanaraksha@gmail.com"
+  const phone="8754046668"
+
   return (
-    <div className='bg-success'>Header</div>
+    <header>
+      <div className='d-flex justify-content-around text-light p-3'>
+      <p><CiMail className='me-1'/>
+        {mail}</p>
+      <p>
+      <MdOutlinePhone className='me-1' />
+      {phone}</p>
+      </div>
+    </header>
   )
 }
 
