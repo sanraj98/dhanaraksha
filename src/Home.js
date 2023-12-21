270ab500bce7a98d0bea1d08886ba752fce123a8
sanraj98 <santhoshrajrathinam@gmail.com>
2023-12-21 09:29:13 +0530

    20-11-23

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,19 @@ import './home.css';
 const Home = () => {
   return (
     <div className='home-body'>
-        <div>hi</div>
+        <div class="background-layer"></div>
+        <div className=''>
+        <div>
+          <h1 className='text-secondary' id="welcome">Teaching Trading course with online Expert</h1>
+          <div className=' position-absolute top-50 start-50 translate-middle'>
+          <h2 className='text-secondary'>Comming soon..!</h2>
+            <div>
+
+            </div>
+          </div>
+        </div>
+
+        </div>
     </div>
   )
 }
